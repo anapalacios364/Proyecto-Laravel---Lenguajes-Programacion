a97265ec0027159cebb05e83e45e15891b8ca2a7
gabriel30102002 <gabrielgue30@gmail.com>
2024-02-20 00:29:11 -0500

    creacion3

--- database/migrations/2024_02_20_033634_create_recetitas_table.php
+++ database/migrations/2024_02_20_033634_create_recetitas_table.php
@@ -12,7 +12,7 @@ class CreateRecetitasTable extends Migration
      */
     public function up()
     {
-    Schema::create('recetitas', function (Blueprint $table) {
+        Schema::create('recetitas', function (Blueprint $table) {
                  $table->increments('id');
                  $table->String('nombre');
                  $table->String('mail');
